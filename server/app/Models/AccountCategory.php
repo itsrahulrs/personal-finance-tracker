da455585150e3d6f98d4550c55b6924b4ed90505
Rahul R S <rahul.rs@webofficeit.com>
2025-02-22 22:35:26 +0530

    implementing soft delete

--- server/app/Models/AccountCategory.php
+++ server/app/Models/AccountCategory.php
@@ -4,15 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AccountCategory extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'name',
         'description'
     ];
 
-
+    protected $dates = ['deleted_at'];
 }
